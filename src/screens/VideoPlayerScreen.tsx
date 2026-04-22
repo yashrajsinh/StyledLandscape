3a5c0771947f7a2de36bdd2bb5c483065d85b334
yash <yashrajsinhjadeja2253@gmail.com>
2026-04-22 14:57:22 +0530

    Works without Styled Components

--- src/screens/VideoPlayerScreen.tsx
+++ src/screens/VideoPlayerScreen.tsx
@@ -1,48 +1,52 @@
-import React, { useRef } from 'react';
-import { ToastAndroid } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, ToastAndroid } from 'react-native';
+import { useVideoPlayer, VideoView, useEvent } from 'react-native-video';
+//styled RN
 import styled from 'styled-components/native';
-import Video, { VideoRef } from 'react-native-video';
 
-type Props = {};
+const VideoPlayerScreen = () => {
+  const player = useVideoPlayer(
+    'https://www.w3schools.com/html/mov_bbb.mp4',
+    _player => {
+      _player.play(); // auto-play
+    },
+  );
 
-const VideoPlayerScreen = (props: Props) => {
-  const videoRef = useRef<VideoRef>(null);
+  useEvent(player, 'onError', error => {
+    ToastAndroid.show('ERROR: ' + error.message, ToastAndroid.LONG);
+  });
+
+  useEvent(player, 'onLoad', () => {
+    ToastAndroid.show('VIDEO LOADED', ToastAndroid.LONG);
+  });
 
   return (
-    <Container>
-      <StyledVideo
-        ref={videoRef}
-        source={{
-          uri: 'https://www.w3schools.com/html/mov_bbb.mp4',
-        }}
-        controls={true}
+    <View style={styles.container}>
+      <VideoView
+        player={player}
+        style={styles.video}
+        controls
         resizeMode="cover"
-        paused={false}
-        onError={e =>
-          ToastAndroid.show(
-            'VIDEO ERROR: ' + e.error?.errorString,
-            ToastAndroid.LONG,
-          )
-        }
-        onLoad={() => ToastAndroid.show('VIDEO LOADED', ToastAndroid.LONG)}
-        onBuffer={({ isBuffering }) => {
-          if (isBuffering) ToastAndroid.show('BUFFERING', ToastAndroid.LONG);
-        }}
       />
-    </Container>
+    </View>
   );
 };
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
+const Cotainer = styled.View`
+ flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
 `;
-
-const StyledVideo = styled(Video)`
-  width: 100%;
-  height: 250px;
-`;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+  video: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+  },
+});
 
 export default VideoPlayerScreen;
